33f61b914bf8e79faebab3d3d64c17ea921ce6d2
Chin-Wei <cw.huang427@gmail.com>
2018-12-19 19:44:54 -0500

    pip setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="torchkit",
+    version="0.0.1",
+    author="Chin-Wei Huang",
+    author_email="chin-wei.huang@umontreal.ca",
+    description="A simple toolkit for variational inference and friends",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
